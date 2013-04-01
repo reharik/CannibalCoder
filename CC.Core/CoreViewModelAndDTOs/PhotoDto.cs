39d0ead6c0e5a5b38b27fc6c54ff53f018ace07e
Raif Harik <reharik@gmail.com>
2013-04-01 12:26:40 -0500

    added id to photodto

--- CC.Core/CoreViewModelAndDTOs/PhotoDto.cs
+++ CC.Core/CoreViewModelAndDTOs/PhotoDto.cs
@@ -2,6 +2,7 @@
 {
     public class PhotoDto
     {
+        public int Id { get; set; }
         public string FileUrl { get; set; }
         public string FileUrl_Thumb { get { return FileUrl.AddImageSizeToName("Thumb"); } }
         public string FileUrl_Large { get { return FileUrl.AddImageSizeToName("Large"); } }
