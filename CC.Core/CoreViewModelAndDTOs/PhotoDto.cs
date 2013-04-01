b33a9e35d1e49c7612914dfaeba5a70f660d4277
Raif Harik <reharik@gmail.com>
2013-04-01 12:47:48 -0500

    added id to photodto

--- CC.Core/CoreViewModelAndDTOs/PhotoDto.cs
+++ CC.Core/CoreViewModelAndDTOs/PhotoDto.cs
@@ -2,7 +2,8 @@
 {
     public class PhotoDto
     {
-        public int Id { get; set; }
+        // fine it's cuz i'm using the imageId as an htmlAttr as well.
+        public int imageId { get; set; }
         public string FileUrl { get; set; }
         public string FileUrl_Thumb { get { return FileUrl.AddImageSizeToName("Thumb"); } }
         public string FileUrl_Large { get { return FileUrl.AddImageSizeToName("Large"); } }
