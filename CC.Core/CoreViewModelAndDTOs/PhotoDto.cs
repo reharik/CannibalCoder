63bbf2997b5add5321c90af1188fbf2e87b7e733
Raif Harik <reharik@gmail.com>
2013-03-31 20:50:25 -0500

    changed photodto

--- CC.Core/CoreViewModelAndDTOs/PhotoDto.cs
+++ CC.Core/CoreViewModelAndDTOs/PhotoDto.cs
@@ -3,6 +3,7 @@
     public class PhotoDto
     {
         public string FileUrl { get; set; }
-        public string FileUrl_Thumb { get { return FileUrl.AddImageSizeToName("Large"); } }
+        public string FileUrl_Thumb { get { return FileUrl.AddImageSizeToName("Thumb"); } }
+        public string FileUrl_Large { get { return FileUrl.AddImageSizeToName("Large"); } }
     }
 }
