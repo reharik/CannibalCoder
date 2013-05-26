c946e6b43802ee0f1a1800382abe8fc721f9831d
Raif Harik <reharik@gmail.com>
2013-05-26 14:40:19 -0500

    test

---
+++ CC.UI.Helpers/TagConfiguration/TagProfileContainer.cs
@@ -0,0 +1,17 @@
+﻿namespace CC.UI.Helpers.Tags
+{
+    public interface ITagProfileContainer
+    {
+        TagProfile Profile { get; set; }
+    }
+
+    public class TagProfileContainer : ITagProfileContainer
+    {
+        private TagProfile _profile;
+        public TagProfile Profile
+        {
+            get { return _profile; }
+            set { _profile = value; }
+        }
+    }
+}
